2aa21f89a46aa1ac2eec97aebf5a23e59547f43d
Scorpio-mfg <htc@myfriendsgroup.com>
2025-01-06 19:43:00 +0530

    upgaring to 3.4.1

--- src/main/java/org/opensearch/data/example/controller/Redirect.java
+++ src/main/java/org/opensearch/data/example/controller/Redirect.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class Redirect {
 
-    @GetMapping(value = "/")
-    public ModelAndView redirectToDocPage() {
-        return new ModelAndView("redirect:/swagger-ui/index.html");
-    }
-
-    @GetMapping(value = "/apidocs")
-    public ModelAndView redirectToApiPage() {
-        return new ModelAndView("redirect:/swagger-ui/index.html");
-    }
+//    @GetMapping(value = "/")
+//    public ModelAndView redirectToDocPage() {
+//        return new ModelAndView("redirect:/swagger-ui/index.html");
+//    }
+//
+//    @GetMapping(value = "/apidocs")
+//    public ModelAndView redirectToApiPage() {
+//        return new ModelAndView("redirect:/swagger-ui/index.html");
+//    }
 }
